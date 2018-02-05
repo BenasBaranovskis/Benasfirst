8837e8494a1a79c92decc17c5d363d1a5644d070
BenasBaranovskis <barbenrok@gmail.com>
2018-02-05 10:49:48 +0200

    Bondapp + newTarget+logged

---
+++ Desktop/Benasfirst/app/src/main/java/k/benasfirst/NewTargetActivity.java
@@ -0,0 +1,13 @@
+package k.benasfirst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewTargetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_target);
+    }
+}
--- Desktop/Benasfirst/app/src/main/java/k/benasfirst/logged.java
+++ Desktop/Benasfirst/app/src/main/java/k/benasfirst/logged.java
@@ -1,13 +1,31 @@
 package k.benasfirst;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class logged extends AppCompatActivity {
+    public Button but2;
 
+    public void init() {
+        but2 = (Button) findViewById(R.id.btnNewEntry);
+        but2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent reg = new Intent(logged.this,NewTargetActivity .class);
+                startActivity(reg);
+
+
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged);
+        init();
     }
 }
